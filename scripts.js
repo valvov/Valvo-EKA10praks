ac6f5359e8811d2d2aa11257361d522741a6b79a
valvov <95522663+valvov@users.noreply.github.com>
2022-03-29 12:03:51 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -117,3 +117,36 @@ mediaButton.onclick = function () {
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
+
+
+/*
+*Carousel
+*/
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
+
+/*
+*menu index
+*
+*/
+$(function() {
+  $('#navbar.navbar-right ul li a').click(function() {
+    //clear active status of any parent LI's
+    $('#navbar.navbar-right ul li').removeClass('active');
+
+    // store id of new active sub-nav
+    var currSub = $(this).parent();
+    currSub.addClass('active')
+    var id = currSub.attr('id');
+
+    // clear active status of any sub-nav list
+    $('#subnavbar ul.navbar-nav').removeClass('active');
+
+    // set selected sub-nav to active
+    $('.' + id).addClass('active');
+
+    console.log($('.' + id).attr('class'));
+
+  });
+
+});
